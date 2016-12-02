4ee6ce83d567da55a3e497b1e5c5f2921c13a5e2
taylorkarras <taylorkarras@gmail.com>
2016-12-02 11:01:33 -0800

    Update ganalytics.php

--- ganalytics/ganalytics.php
+++ ganalytics/ganalytics.php
@@ -13,5 +13,31 @@ class ganalytics extends plugin {
         echo $analyticscode2['analytics_code'];
         return true;
     }
+	
+    static function amp_bottom(){
+                $global = new DB_global;
+                $analyticscode1 = $global->sqlquery("SELECT * from ddp_ganalytics");
+                $analyticscode2 = $analyticscode1->fetch_assoc();
+preg_match("/(['])UA-.*?(['])/", $analyticscode2['analytics_code'], $analyticsnumber);
+$analyticsnumber1 = str_replace("'", '', $analyticsnumber);
+echo '  <amp-analytics type="googleanalytics">
+    <script type="application/json">
+      {
+        "vars": {
+          "account": "'.$analyticsnumber1[0].'"
+        },
+        "triggers": {
+          "default pageview": {
+            "on": "visible",
+            "request": "pageview",
+            "vars": {
+              "title": "{{title}}"
+            }
+          }
+        }
+      }
+    </script>
+  </amp-analytics>';
+}
 }
 ?>
