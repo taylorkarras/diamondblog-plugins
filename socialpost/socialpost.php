1d3e0613c3d92d37bdacaaa9a0d753051d6d6b77
taylorkarras <taylorkarras@gmail.com>
2016-12-10 11:47:20 -0800

    Update socialpost.php

--- socialpost/socialpost.php
+++ socialpost/socialpost.php
@@ -12,13 +12,26 @@ if( !defined( "INPROCESS" ) ){
 
 class socialpost extends plugin {
 
+    static function inc_post_form_bottom_error(){
+		if(isset($_POST['posttotwitter']) && $_POST['posttotwitter'] == '1' && strlen('New in "'.$_POST['category'].'" - '.$GLOBALS['posttitle'].' https://'.$_SERVER['HTTP_HOST'].'/'.$GLOBALS['shortlink'].' '.$_POST['twitterextra']) > 140)  {
+		$_SESSION['errors']['twitterextra'] = "Twitter post cannot be longer than 140 characters.";
+		$hasError = true;	
+	}
+	
+			if(isset($hasError)){
+		if(!empty($_SERVER['HTTP_X_REQUESTED_WITH']) &&  strtolower($_SERVER['HTTP_X_REQUESTED_WITH']) == 'xmlhttprequest') {
+                echo json_encode($_SESSION['errors']);
+                exit;
+	}}
+	}
+	
     static function inc_post_form_bottom(){
 				$global = new DB_global;
 		$social1 = $global->sqlquery("SELECT * FROM ddp_socialpost");
 		$social2 = $social1->fetch_assoc();
 				if (isset($_POST['posttotwitter']) && $_POST['posttotwitter'] == '1'){
 		$tconnection = new TwitterOAuth($social2['twitter_apikey'], $social2['twitter_apisecret'], $social2['twitter_accesstoken'], $social2['twitter_accesstokensecret']);
-		$tconnection->post("statuses/update", ["status" => 'New in "'.$_POST['category'].'" - '.$GLOBALS['posttitle'].' https://'.$_SERVER['HTTP_HOST'].'/'.$GLOBALS['shortlink'].' #vaporwave']);
+		$tconnection->post("statuses/update", ["status" => 'New in "'.$_POST['category'].'" - '.$GLOBALS['posttitle'].' https://'.$_SERVER['HTTP_HOST'].'/'.$GLOBALS['shortlink'].' '.$_POST['twitterextra']]);
 				}
 				if (isset($_POST['posttofacebook']) && $_POST['posttofacebook'] == '1'){
 		$fb = new Facebook\Facebook([
@@ -87,10 +100,34 @@ $pinterest->pins->create(array(
 		$ifenabled2 = $ifenabled1->fetch_assoc();
         echo '<div class="sitescrolling">';
 		if($ifenabled2['twitter_enabled'] == '1'){
-		echo '<br><input type="checkbox" name="posttotwitter" value="1" checked> Post to Twitter';
+		echo '<br><input type="checkbox" name="posttotwitter" value="1" checked> Post to Twitter</div>';
+		echo '<script>$( document ).ready(function() {
+			$("textarea#twitterexample").val("New in \"" + $("#post select[name=category]").val() + "\" - " + $("#post input[name=posttitle]").val() + " https://'.$_SERVER['HTTP_HOST'].'/examplexxx " + $("#post input[name=twitterextra]").val());
+						    var left = 140 - $("textarea#twitterexample").val().length;
+    if (left < 0) {
+        left = 0;
+    }
+    $(".countdown").text("Characters left: " + left);
+		})
+		$( "#post" ).bind("change keyup", function(event){
+		$("textarea#twitterexample").val("New in \"" + $("#post select[name=category]").val() + "\" - " + $("#post input[name=posttitle]").val() + " https://'.$_SERVER['HTTP_HOST'].'/examplexxx "  + $("#post input[name=twitterextra]").val()) ;
+    var left = 140 - $("textarea#twitterexample").val().length;
+    if (left < 0) {
+        left = 0;
+    }
+    $(".countdown").text("Characters left: " + left);
+	});</script>';
+		echo '<br><textarea style="width:100%; height:50px; color:#000; background-color:white; resize:none;" disabled id="twitterexample"></textarea>';
+		echo '<span class="countdown" style="font-size: 20px;"></span>';
+		$twitterextra = '';
+		if (isset($ifenabled2['twitter_extradefault'])){
+		$twitterextra='value="'.$ifenabled2['twitter_extradefault'].'"';
+		}
+		echo '<br><br><label title="twitterextra"><b>Add to Twitter post:</b></label>
+		<br><input type="text" name="twitterextra" '.$twitterextra.'>';
 				}
 		if($ifenabled2['facebook_enabled'] == '1'){
-		echo '<br><input type="checkbox" name="posttofacebook" value="1" checked> Post to Facebook';
+		echo '<br><div class="sitescrolling"><br><input type="checkbox" name="posttofacebook" value="1" checked> Post to Facebook';
 		}
 				if($ifenabled2['pinterest_enabled'] == '1'){
 		echo '<br><input type="checkbox" name="posttopinterest" value="1" checked> Post to Pinterest';
