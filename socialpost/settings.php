2936fbf0511f742050abfc0674eff5bf262781b3
taylorkarras <taylorkarras@gmail.com>
2016-12-10 12:41:27 -0800

    Update settings.php

--- socialpost/settings.php
+++ socialpost/settings.php
@@ -13,16 +13,16 @@ echo '<div class="sitescrolling">
 	echo 'value="0"';
 }echo'> Twitter enabled<br></div>'; echo'
 <br /><label name="twitterapikey">Twitter API Key</label>
-<br /><input type="text" name="twitterapikey" value='.$data['twitter_apikey'].'/>
+<br /><input type="text" name="twitterapikey" value='.$data['twitter_apikey'].'>
 <br /><label name="twitterapisecret">Twitter API Secret</label>
-<br /><input type="text" name="twitterapisecret" value="'.$data['twitter_apisecret'].'" />
+<br /><input type="text" name="twitterapisecret" value="'.$data['twitter_apisecret'].'">
 <br /><label name="twitteraccesstoken">Twitter Access Token</label>
-<br /><input type="text" name="twitteraccesstoken" value="'.$data['twitter_accesstoken'].'"/>
+<br /><input type="text" name="twitteraccesstoken" value="'.$data['twitter_accesstoken'].'">
 <br /><label name="twitteraccesssecret">Twitter Access Secret</label>
-<br /><input type="text" name="twitteraccesssecret" value="'.$data['twitter_accesstokensecret'].'"/>
+<br /><input type="text" name="twitteraccesssecret" value="'.$data['twitter_accesstokensecret'].'">
 <br /><label name="twitteraccesssecret">Twitter Extra Default</label>
-<br /><input type="text" name="twitterextradefault" value="'.$data['twitter_extradefault'].'"/>
-<input type="hidden" name="twitterdistinguish" value="1" />
+<br /><input type="text" name="twitterextradefault" value="'.$data['twitter_extradefault'].'">
+<input type="hidden" name="twitterdistinguish" value="1">
 <br /><br /><input class="postsubmit" name="twittersubmit" type="submit" value="Submit">
 </form>';
 echo '<form id="pluginsettings2" method="post">';
@@ -33,15 +33,15 @@ echo '<div class="sitescrolling">
 	echo 'value="0"';
 }echo'> Facebook enabled<br></div>'; echo'
 <br /><label name="facebookapikey">Facebook API Key</label>
-<br /><input type="text" name="facebookapikey" value='.$data['facebook_apikey'].'/>
+<br /><input type="text" name="facebookapikey" value='.$data['facebook_apikey'].'>
 <br /><label name="facebookapisecret">Facebook API Secret</label>
-<br /><input type="text" name="facebookapisecret" value="'.$data['facebook_apisecret'].'" />
+<br /><input type="text" name="facebookapisecret" value="'.$data['facebook_apisecret'].'">
 <br /><label name="facebookaccesstoken">Facebook Access Token</label>
-<br /><input type="text" name="facebookaccesstoken" value="'.$data['facebook_accesstoken'].'"/>
+<br /><input type="text" name="facebookaccesstoken" value="'.$data['facebook_accesstoken'].'">
 <div class="smalltext"><a href="https://developers.facebook.com/tools/explorer/" title="Get access token here." alt="Get access token here.">Get access token here.</a></div>
 <label name="facebookpagename">Facebook Page Name</label>
-<br /><input type="text" name="facebookpagename" value="'.$data['facebook_pagename'].'"/>
-<input type="hidden" name="facebookdistinguish" value="1" />
+<br /><input type="text" name="facebookpagename" value="'.$data['facebook_pagename'].'">
+<input type="hidden" name="facebookdistinguish" value="1">
 <br /><br /><input class="postsubmit" name="facebooksubmit" type="submit" value="Submit">
 </form>';
 echo '<form id="pluginsettings3" method="post">';
@@ -52,15 +52,15 @@ echo '<div class="sitescrolling">
 	echo 'value="0"';
 }echo'> Pinterest enabled<br></div>'; echo'
 <br /><label name="pinterestapikey">Pinterest API Key</label>
-<br /><input type="text" name="pinterestapikey" value='.$data['pinterest_apikey'].'/>
+<br /><input type="text" name="pinterestapikey" value='.$data['pinterest_apikey'].'>
 <br /><label name="pinterestapisecret">Pinterest API Secret</label>
-<br /><input type="text" name="pinterestapisecret" value="'.$data['pinterest_apisecret'].'" />
+<br /><input type="text" name="pinterestapisecret" value="'.$data['pinterest_apisecret'].'">
 <br /><label name="pinterestaccesstoken">Pinterest Access Token</label>
-<br /><input type="text" name="pinterestaccesstoken" value="'.$data['pinterest_token'].'"/>
+<br /><input type="text" name="pinterestaccesstoken" value="'.$data['pinterest_token'].'">
 <div class="smalltext"><a href="https://developers.pinterest.com/tools/access_token/" title="Get access token here." alt="Get access token here.">Get access token here.</a></div>
 <label name="facebookpagename">Pinterest Board for Pinning</label>
-<br /><input type="text" name="pinterestboard" value="'.$data['pinterest_board'].'"/>
-<input type="hidden" name="pinterestdistinguish" value="1" />
+<br /><input type="text" name="pinterestboard" value="'.$data['pinterest_board'].'">
+<input type="hidden" name="pinterestdistinguish" value="1">
 <br /><br /><input class="postsubmit" name="pinterestsubmit" type="submit" value="Submit">
 </form>
 </div></div>';
