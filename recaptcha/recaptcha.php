478a8021e0fbbb07e1e66ba856a6ae7fcd1bcacc
taylorkarras <taylorkarras@gmail.com>
2016-12-26 17:05:02 -0800

    Update recaptcha.php

--- recaptcha/recaptcha.php
+++ recaptcha/recaptcha.php
@@ -11,9 +11,21 @@ class recaptcha extends plugin {
         echo "<script src='https://www.google.com/recaptcha/api.js'></script>";
         return true;
     }
+
+static function user_contact(){
+        echo "<script src='https://www.google.com/recaptcha/api.js'></script>";
+        return true;
+    }
+
 	
     static function page_top(){
         echo "<script src='https://www.google.com/recaptcha/api.js'></script>";
+        return true;
+    }
+	
+    static function ampcomments_captcha(){
+        echo "<script src='https://www.google.com/recaptcha/api.js'
+        async defer></script>";
         return true;
     }
 	
@@ -22,11 +34,14 @@ class recaptcha extends plugin {
 		$recaptcha1 = $global->sqlquery("SELECT * from ddp_recaptcha");
 		$recaptcha2 = $recaptcha1->fetch_assoc();
         echo '<div class="g-recaptcha" data-sitekey="'.$recaptcha2['recaptcha_sitekey'].'"></div>';
+		echo '<input type="hidden" name="captcha">';
         return true;
     }
 	
     static function captcha(){
 		$global = new DB_global;
+		$check = new DB_check;
+		if (!$check->isLoggedIn()){	
 		$recaptcha1 = $global->sqlquery("SELECT * from ddp_recaptcha");
 		$recaptcha2 = $recaptcha1->fetch_assoc();
 		$secret = $recaptcha2['recaptcha_secret'];
@@ -36,10 +51,18 @@ $captcha_success=json_decode($verify);
 
 
 if ($captcha_success->success==false) {
-	$_SESSION['errors']['commentip'] = "Please successfully complete the captcha.";
+	$_SESSION['errors']['captcha'] = "Please successfully complete the captcha.";
 	$hasError = true;	
 }
+
+		if(isset($hasError)){
+		if(!empty($_SERVER['HTTP_X_REQUESTED_WITH']) &&  strtolower($_SERVER['HTTP_X_REQUESTED_WITH']) == 'xmlhttprequest') {
+                echo json_encode($_SESSION['errors']);
+                exit;
+	}}
+
         return true;
     }
+	}
 }
 ?>
