c633c779cf66c33e4b95a3a3df675db5933877a1
taylorkarras <taylorkarras@gmail.com>
2017-01-01 23:17:40 -0800

    Update instagramfunctions.php

--- socialpost/socialpostconsole/includes/instagramfunctions.php
+++ socialpost/socialpostconsole/includes/instagramfunctions.php
@@ -18,7 +18,7 @@ echo '<html>
 <head>
 <meta name="robots" content="noindex">
 <link href="https://'.$_SERVER['HTTP_HOST'].'/plugins/socialpost/socialpostconsole/styles/instagram.css" rel="stylesheet" type="text/css">
-<script src="https://'.$_SERVER['HTTP_HOST'].'/includes/console/scripts/jquery-2.2.3.min.js"></script>
+<script src="https://'.$_SERVER['HTTP_HOST'].'/scripts/jquery-2.2.3.min.js"></script>
 <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
 </head>
 <body><div id="gramcomments" style="padding:15px;">';
@@ -83,6 +83,87 @@ function loadcomments() {
 			}</script></div>";
  }
 }
+echo '</div></body>
+</html>';
+exit;
+}
+
+if(isset($_GET['dmid'])){
+echo '<html>
+<head>
+<meta name="robots" content="noindex">
+<link href="https://'.$_SERVER['HTTP_HOST'].'/plugins/socialpost/socialpostconsole/styles/instagram.css" rel="stylesheet" type="text/css">
+<script src="https://'.$_SERVER['HTTP_HOST'].'/scripts/jquery-2.2.3.min.js"></script>
+<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
+</head>
+<body><div id="gramcomments" class="dmoverflow" style="padding:15px;">';
+$dm = json_decode(json_encode($instagram->directthread($_GET['dmid'])), true);
+$owndm = json_decode(json_encode($instagram->getSelfUsernameInfo()), true);
+var_dump($dm);
+ foreach ($dm['thread']['items'] as $dmitem){
+if (!empty($dmitem["media_share"]["image_versions2"]["candidates"][0])){
+$item = '<img class="dmimage" src='.$dmitem["media_share"]["image_versions2"]["candidates"][0]['url'].'>';
+} else if ($dmitem["item_type"] == 'like'){
+$item = '&#10084';
+} else {
+$item = $dmitem['text'];
+}
+if (!empty($dm['thread']["users"])){
+$userpic = $dm['thread']['users'][0]['profile_pic_url'];
+$username = $dm['thread']['users'][0]['username'];
+$userpk = $dm['thread']['users'][0]['pk'];
+} else {
+$userpic = $dm['thread']["inviter"]['profile_pic_url'];
+$username = $dm['thread']["inviter"]['username'];
+$userpk = $dm['thread']["inviter"]['pk'];
+}
+if($dmitem['user_id'] == $owndm["user"]["pk"]){
+			echo '<div class="gramcomment2"><img class="gramavatar" src="'.$owndm["user"]["profile_pic_url"].'"><strong>'.$owndm["user"]["username"].':</strong> '.$item.'</div>';
+} else {
+			echo '<div class="gramcomment2"><img class="gramavatar" src="'.$userpic.'"><strong>'.$username.':</strong> '.$item.'</div>';
+}
+			}
+echo '</div>';
+echo '<form id="dmsend">
+<textarea name="dmbox" id="dmbox"></textarea>
+<input type="hidden" name="dmrecp" value="'.$userpk.'">
+<input type="submit" name="dmsubmit" style="display:none;">
+</form>';
+echo "<script>var data = {};
+$(function() {
+$('#dmsend').keypress(function (e) {
+        if(e.which == 13) {
+	  var myinstances = [];
+	  $('input, button, select, textarea').prop('disabled', true);
+      $.each($('form input, form select'), function(i, v) {
+          if (v.type !== 'submit') {
+              data[v.name] = v.value;
+          }
+	  }); $.each($('textarea'), function(i, v) {
+          if (v.type !== 'submit') {
+              data[v.name] = v.value;
+          }
+      });
+	  //end each
+     $.ajax({
+          dataType: 'json',
+          type: 'POST',
+          url: '/console/social/instagram/comment',
+          data: data,
+          success: function(resp) {
+              if (resp.success === true) {
+				  window.location.reload(true);
+			  }
+          },
+          error: function(xhr, status, error) {
+			  var err = eval('(' + xhr.responseText + ')');
+              console.log(err.Message);
+          }
+      }); 
+      return false;
+}});
+});
+</script>";
 echo '</div></body>
 </html>';
 exit;
