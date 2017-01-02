cb71aeb91bedbf1fb1275b8518cb016f0ac01401
taylorkarras <taylorkarras@gmail.com>
2017-01-01 23:17:58 -0800

    Update instagramcomment.php

--- socialpost/socialpostconsole/includes/instagramcomment.php
+++ socialpost/socialpostconsole/includes/instagramcomment.php
@@ -6,9 +6,14 @@ $global = new DB_global;
 if ($retrive->isLoggedIn() == true && $social2['instagram_enabled'] == '1' && $social2['console_enabled'] == '1'){
     if (isset($_POST))
 {
+if (isset($_POST['dmbox'])){
+	$id = $_POST['dmrecp'];
+	$comment = $_POST['dmbox'];
+} else {
 	$comment = $_POST['gramcommentbox'];
 	$id = $_POST['gramcommentid'];
-
+}
+	
 require($_SERVER['DOCUMENT_ROOT']."/plugins/socialpost/vendor/autoload.php");
 $debug = false;
 $truncatedDebug = false;
@@ -17,6 +22,20 @@ $username = $social2['instagram_username'];
 $password = $social2['instagram_password'];
 $instagram->setUser($username, $password);
 $instagram->login();
+
+if (isset($_POST['dmbox'])){
+$instagram->direct_message($id, $comment);
+
+
+							        		if(!empty($_SERVER['HTTP_X_REQUESTED_WITH']) &&  strtolower($_SERVER['HTTP_X_REQUESTED_WITH']) == 'xmlhttprequest') {
+			
+				$resp = array();
+				$resp['success'] = true;
+			
+                echo json_encode($resp);
+		        exit;
+	}
+}
 
 $instagram->comment($id, $comment);
 
